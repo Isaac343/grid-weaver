d40d6fc4aa897b615c00f81868cf99958ad051c4
Milton Olaf Paredes <miltonolaf@outlook.com>
2018-10-01 22:51:44 -0500

    frontend elements

---
+++ gw-admin/views/index/index.php
@@ -0,0 +1,38 @@
+<div class="page-single">
+	<div class="container">
+		<div class="row">
+			<div class="col col-login mx-auto">
+				<div class="text-center mb-6">
+				</div>
+				<form class="card" action="" method="post">
+					<div class="card-body p-6">
+						<div class="card-title">Ingresa a tu cuenta</div>
+						<div class="form-group">
+							<label class="form-label">Nombre de usuario</label>
+							<input class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Usuario" type="text" required>
+						</div>
+						<div class="form-group">
+							<label class="form-label">
+								Contraseña
+								<a href="./forgot-password.html" class="float-right small">I forgot password</a>
+							</label>
+							<input class="form-control" id="exampleInputPassword1" placeholder="Contraseña" type="password" required>
+						</div>
+						<div class="form-group">
+							<label class="custom-control custom-checkbox">
+								<input class="custom-control-input" type="checkbox">
+								<span class="custom-control-label">Recuerdame</span>
+							</label>
+						</div>
+						<div class="form-footer">
+							<button type="submit" class="btn btn-primary btn-block">Entrar</button>
+						</div>
+					</div>
+				</form>
+				<div class="text-center text-muted">
+					Don't have account yet? <a href="./register.html">Sign up</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
