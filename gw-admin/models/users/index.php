b987d26201765a54667ef706ef6a64a284fbf8b5
Isaac <izaak343@gmail.com>
2018-10-13 20:31:07 -0500

    files deleted

--- gw-admin/models/users/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once ROOT . 'config/connection.php';
-$sql = "SELECT * FROM `users` WHERE `user_rank` = 0 ";
-$result = mysqli_query($conn, $sql);
-
-$sql_emp = "SELECT * FROM `users` WHERE `user_rank` = 1 ";
-$result_admin = mysqli_query($conn, $sql_emp);
- ?>
