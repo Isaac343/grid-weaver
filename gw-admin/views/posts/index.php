c9cf2184ec81bfd037f08b356af77321814be2e5
Isaac <izaak343@gmail.com>
2018-11-13 23:13:10 -0600

    posts admin side added

---
+++ gw-admin/views/posts/index.php
@@ -0,0 +1,15 @@
+<div class="row">
+	<?php include ROOT . 'views/_includes/sidebar.php' ?>
+	<div class="col-10">
+		<div class="row justify-content-center">
+			<div class="col-5">
+				<div class="card">
+
+        </div>
+			</div>
+			<div class="col-5">
+
+			</div>
+		</div>
+	</div>
+</div>
