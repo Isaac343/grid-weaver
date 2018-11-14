0b6cfb53ff42ae35ac7fe5d58d99574ef3a716fd
Isaac <izaak343@gmail.com>
2018-11-13 23:12:06 -0600

    conection root update

--- gw-admin/models/index/index.php
+++ gw-admin/models/index/index.php
@@ -1,11 +1,11 @@
 <?php
-	require ROOT . 'config/connection.php';
+	require ROOT . '/models/connection.php';
 
 	if (isset($_POST['login_send'])) {
 		$username = $_POST['username'];
 		$password = $_POST['password'];
 
-		$sql = "SELECT IdUser, user_name, user_lastname, user_password, user_area, user_rank FROM users WHERE user_username = '{$username}'";
+		$sql = "SELECT IdUser, user_name, user_lastname, user_password, user_area, user_rank FROM gw_users WHERE user_username = '{$username}'";
 		$result = mysqli_query($conn, $sql);
 
 		if (mysqli_num_rows($result) > 0) {
