bca7d7805018f53dbabce483152f930be7c32b4e
Isaac <izaak343@gmail.com>
2018-11-24 21:49:00 -0600

    views - users edited

--- gw-admin/views/users/add.php
+++ gw-admin/views/users/add.php
@@ -3,35 +3,42 @@
 	<div class="col-10">
 		<div class="row justify-content-center">
 			<div class="col-5">
-				<div class="shadow-lg py-4 px-5 mt-8 bg-white rounded">
+				<div class="new-user-form shadow-lg py-4 px-5 mt-8 rounded ">
 					<form action="" method="POST">
-						<h3 class="text-center">New user</h3>
+						<h3 class="text-center admin-form">New user</h3>
 						<div class="form-group">
-							<input class="form-control font-weight-bold" type="text" name="username" placeholder="Username">
+							<input class="admin-form-input font-weight-bold" type="text" name="username" placeholder="Username">
 						</div>
 						<div class="form-group">
-						  <input class="form-control font-weight-bold" type="text" name="name" placeholder="Name">
+						  <input class="admin-form-input font-weight-bold" type="text" name="name" placeholder="Name">
 						</div>
 					  <div class="form-group">
-						 	<input class="form-control font-weight-bold" type="text" name="surname" placeholder="Surname">
+						 	<input class="admin-form-input font-weight-bold" type="text" name="surname" placeholder="Surname">
 					  </div>
 					 	<div class="form-group">
-						  	<input class="form-control font-weight-bold" type="text" name="email" placeholder="E-mail">
+						  	<input class="admin-form-input font-weight-bold" type="text" name="email" placeholder="E-mail">
 					 	</div>
 					 	<div class="form-group">
-						 	<input class="form-control font-weight-bold" type="text" name="rol" placeholder="Rol">
+						 	<input class="admin-form-input font-weight-bold" type="text" name="rol" placeholder="Rol">
 					 	</div>
 						<div class="form-group">
-							<input class="form-control font-weight-bold" type="password" name="password" placeholder="Password">
+							<input class="admin-form-input font-weight-bold" type="password" name="password" placeholder="Password">
 						</div>
 						<div class="form-group">
-							<button type="submit"  class="btn btn-primary btn-lg" name="new_user" value="1">Create</button>
+							<button type="submit"  class="btn admin-add-button btn-lg" name="new_user" value="1">Create</button>
 						</div>
 					</form>
 				</div>
 			</div>
 			<div class="col-5">
-
+				<div class="new-user-form shadow-lg py-4 px-5 mt-8 rounded ">
+					<form action="" method="POST">
+						<h3 class="text-center admin-form">Users</h3>
+						<div class="form-group">
+							<input type="text" class="admin-form-input font-weight-bold" name="Userdel" placeholder="Select user">
+						</div>
+					</form>
+				</div>
 			</div>
 		</div>
 	</div>
---
+++ gw-admin/views/users/index.php
@@ -0,0 +1,45 @@
+<div class="row admin-part">
+	<?php include ROOT . 'views/_includes/sidebar.php' ?>
+	<div class="col-10">
+		<div class="row justify-content-center">
+			<div class="col-5">
+				<div class="new-user-form shadow-lg py-4 px-5 mt-8 rounded ">
+					<form action="" method="POST">
+						<h3 class="text-center admin-form">New user</h3>
+						<div class="form-group">
+							<input class="admin-form-input font-weight-bold" type="text" name="username" placeholder="Username">
+						</div>
+						<div class="form-group">
+						  <input class="admin-form-input font-weight-bold" type="text" name="name" placeholder="Name">
+						</div>
+					  <div class="form-group">
+						 	<input class="admin-form-input font-weight-bold" type="text" name="surname" placeholder="Surname">
+					  </div>
+					 	<div class="form-group">
+						  	<input class="admin-form-input font-weight-bold" type="text" name="email" placeholder="E-mail">
+					 	</div>
+					 	<div class="form-group">
+						 	<input class="admin-form-input font-weight-bold" type="text" name="rol" placeholder="Rol">
+					 	</div>
+						<div class="form-group">
+							<input class="admin-form-input font-weight-bold" type="password" name="password" placeholder="Password">
+						</div>
+						<div class="form-group">
+							<button type="submit"  class="btn admin-add-button btn-lg" name="new_user" value="1">Create</button>
+						</div>
+					</form>
+				</div>
+			</div>
+			<div class="col-5">
+				<div class="new-user-form shadow-lg py-4 px-5 mt-8 rounded ">
+					<form action="" method="POST">
+						<h3 class="text-center admin-form">Users</h3>
+						<div class="form-group">
+							<input type="text" class="admin-form-input font-weight-bold" name="Userdel" placeholder="Select user">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
