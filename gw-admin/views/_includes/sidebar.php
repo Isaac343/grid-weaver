23268b0ad205e89ba7799cb6eedb36121bae39ea
Isaac <izaak343@gmail.com>
2018-11-24 22:07:39 -0600

    admin views - sidebar update

--- gw-admin/views/_includes/sidebar.php
+++ gw-admin/views/_includes/sidebar.php
@@ -4,12 +4,31 @@
     Grid Weaver
   </div>
   <ul>
-    <li><a href="">Home</a></li>
-    <li><a href="">Posts</a></li>
-    <li><a href="">Pages</a></li>
-    <li><a href="">Appearance</a> </li>
-    <li><a href="">Users</a></li>
-    <li><a href="">Settings</a></li>
-    <li><a href="">Media</a></li>
+    <li><a href="<?php echo URL; ?>dashboard">Home</a></li>
+    <details>
+      <summary>Posts</summary>
+      <ul>
+        <li><a href="<?php echo URL; ?>posts/overall">All posts</a></li>
+        <li><a href="<?php echo URL; ?>posts/new-post">New post</a></li>
+      </ul>
+    </details>
+    <details>
+      <summary>Pages</summary>
+      <ul>
+        <li><a href="<?php echo URL; ?>pages">All pages</a></li>
+        <li><a href="<?php echo URL; ?>pages">New pages</a></li>
+      </ul>
+    </details>
+    <li><a href="<?php echo URL; ?>appareance">Appearance</a> </li>
+    <details>
+      <summary>Users</summary>
+      <ul>
+        <li><a href="<?php echo URL; ?>users">All users</a></li>
+        <li><a href="<?php echo URL; ?>users/add">Add user</a></li>
+        <li><a href="<?php echo URL; ?>users/edit-user">Edit user</a></li>
+      </ul>
+    </details>
+    <li><a href="<?php echo URL; ?>settings">Settings</a></li>
+    <li><a href="<?php echo URL; ?>media">Media</a></li>
   </ul>
 </div>
