717543864113ab4988aa50d0de4995ed5f77f001
Isaac <izaak343@gmail.com>
2018-11-24 21:51:33 -0600

    view not founded

---
+++ gw-admin/views/unknown/index.php
@@ -0,0 +1,13 @@
+<div class="row not-found">
+  <h1>PAGE NOT FOUND</h1>
+  <div class="lost-message">
+    <h3 class="lost-messge-title">Sorry...</h3>
+    <p>We are having trouble findig the page you need.<br>
+    This may be because:
+      <ul>
+        <li>Page dosen't exist</li>
+        <li>The URL is wrong</li>
+      </ul>
+    </p>
+  </div>
+</div>
