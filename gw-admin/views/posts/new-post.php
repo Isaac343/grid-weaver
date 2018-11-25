087a7b2ed1eb0bb85a6e7730c4185d316669a62c
Isaac <izaak343@gmail.com>
2018-11-24 21:54:19 -0600

    admin views - post overall and new post added

---
+++ gw-admin/views/posts/new-post.php
@@ -0,0 +1,25 @@
+<div class="row admin-part">
+	<?php include ROOT . 'views/_includes/sidebar.php' ?>
+	<div class="col-10">
+		<div class="row justify-content-center">
+			<div class="col-8">
+				<div class="new-user-form shadow-lg py-4 px-5 mt-8 rounded">
+					<h3>Add new posts</h3>
+          <form action="" method="post">
+            <div class="form-group">
+              <input type="text"  class="admin-form-input font-weight-bold" name="post-title" placeholder="Insert post title here...">
+            </div>
+          </form>
+				</div>
+			</div>
+      <div class="col-2">
+        <div class="new-user-form shadow-lg py-4 px-5 mt-8 rounded">
+          <h4>Post options</h4>
+
+
+        </div>
+
+      </div>
+		</div>
+	</div>
+</div>
---
+++ gw-admin/views/posts/overall.php
@@ -0,0 +1,29 @@
+<div class="row admin-part">
+	<?php include ROOT . 'views/_includes/sidebar.php' ?>
+	<div class="col-10">
+		<div class="row justify-content-center">
+			<div class="col">
+				<div class="new-user-form shadow-lg py-4 px-5 mt-8 rounded ">
+					<h3>Posts</h3>
+					<table class="table">
+						<thead>
+							<tr>
+								<th scope="col">#</th>
+								<th scope="col">Title</th>
+								<th scope="col">Author</th>
+								<th scope="col">Categories</th>
+								<th scope="col">Tags</th>
+								<th scope="col">Date</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+								
+							?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
